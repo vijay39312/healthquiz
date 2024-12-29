6b38e42098813766313d5635c9bc6c6143c4e7d6
Nidhi Patnaik <nidhi@Nidhis-MacBook-Air.local>
2024-12-29 10:27:47 +0530

    Changed statements

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3f21f911a5fae5a"; }
+/******/ 		__webpack_require__.h = function() { return "9ddd3037e3e6b872"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- data/statements.js
+++ data/statements.js
@@ -1,35 +1,16 @@
-export const correctStatements = [
-    "Correct statement 1",
-    "Correct statement 2",
-    "Correct statement 3",
-    "Correct statement 4",
-    "Correct statement 5",
-    "Correct statement 6",
-    "Correct statement 7",
-    "Correct statement 8",
-    "Correct statement 9",
-    "Correct statement 10",
-    "Correct statement 11",
-    "Correct statement 12",
-    "Correct statement 13",
-    "Correct statement 14",
-    "Correct statement 15",
-    "Correct statement 16",
-    "Correct statement 17",
-    "Correct statement 18",
-    "Correct statement 19",
-    "Correct statement 20"
-  ];
-  
-  export const incorrectStatements = [
-    "Incorrect statement 1",
-    "Incorrect statement 2",
-    "Incorrect statement 3",
-    "Incorrect statement 4",
-    "Incorrect statement 5",
-    "Incorrect statement 6",
-    "Incorrect statement 7",
-    "Incorrect statement 8",
-    "Incorrect statement 9",
-    "Incorrect statement 10"
-  ];
+export const correctStatements = ["Your liver can regenerate itself completely even if 70% is removed.",
+    "Depression can cause physical symptoms like chronic pain and digestive issues.",
+    "Anesthesia doesn’t make you unconscious. It prevents your brain from forming memories.",
+    "Your stomach gets a new lining every 3-4 days to prevent it from digesting itself.",
+    "Panic attacks can occur even when you are completely calm.",
+    "Alzheimer’s disease begins affecting the brain upto 20 yrs before symptoms appear.",
+    "Your heartbeat synchronises to the music you are listening to.",
+    "Humans have more bacterial cells in their body than human cells.",
+    "Your body produces enough saliva in a year to fill two bathtubs.",
+    "Endorphins are stronger than morphine when it comes to pain."];
+
+  export const incorrectStatements = ["Cravings mean your body needs a specific nutrient.",
+    "A resting heart rate of 60-100 bpm is always healthy.",
+    "If you are not thirsty you are not dehydrated.",
+    "If someone faints, slap their face to wake them up.",
+    "You can sweat out toxins in sauna."]
